b900ec8a2bc10f5394f973a2eba428f2508f161b
Aryan Gupta <aryangupta02092002@gmail.com>
2022-11-23 12:58:45 +0530

    Leetcode Nov 23

---
+++ 36_ValidSudoku.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+class Sol{
+    public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> s;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }
+                auto it = s.find(board[i][j]);
+                if(it != s.end()){
+                    return false;
+                }
+                else{
+                    s.insert(board[i][j]);
+                }
+            }
+            s.clear();
+        }
+        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[j][i] == '.'){
+                    continue;
+                }
+                auto it = s.find(board[j][i]);
+                if(it != s.end()){
+                    return false;
+                }
+                else{
+                    s.insert(board[j][i]);
+                }
+            }
+            s.clear();
+        }
+        
+        for(int iCount=0; iCount<9; iCount+=3){
+            for(int jCount=0; jCount<9; jCount+=3){
+                for(int i=iCount; i<iCount+3; i++){
+                    for(int j=jCount; j<jCount+3; j++){
+                        if(board[i][j] == '.'){
+                            continue;
+                        }
+                        auto it = s.find(board[i][j]);
+                        if(it != s.end()){
+                            return false;
+                        }
+                        else{
+                            s.insert(board[i][j]);
+                        }
+                    }
+                }
+                s.clear();
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    int t;  cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<vector<char>> board(n, vector<char> (n));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                char ch;
+                cin>>ch;
+                board[i][j] = ch;
+            }
+        }
+        Sol s;
+        cout<<s.isValidSudoku(board)<<endl;
+    }
+    
+}
