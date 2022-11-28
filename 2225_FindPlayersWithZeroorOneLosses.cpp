fc5c6d91d2e076a289f5bad1843dc11e6078e414
Aryan Gupta <aryangupta02092002@gmail.com>
2022-11-28 14:54:59 +0530

    Leetcode Nov 28

---
+++ 2225_FindPlayersWithZeroorOneLosses.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Sol{
+    public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches){
+        map<int, int> m;
+        for(int i=0; i<matches.size(); i++){
+            if(!m.count(matches[i][0])){
+                m[matches[i][0]] = 0;
+            }
+            m[matches[i][1]]++;
+        }
+        vector<int> zero, one;
+        for(auto [key, val] : m){
+            if(val == 0){
+                zero.push_back(key);
+            }
+            if(val == 1){
+                one.push_back(key);
+            }
+        }
+        return {zero, one};
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> matches;
+    for(int i=0; i<n; i++){
+        int a, b;
+        cin>>a>>b;
+        matches.push_back({a, b});
+    }
+    Sol s;
+    vector<vector<int>> ans = s.findWinners(matches);
+    for(int i=0; i<ans.size(); i++){
+        for(int j=0; j<ans[i].size(); j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
